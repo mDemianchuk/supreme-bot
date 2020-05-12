956e85f5cc8b7725173b3a85e3c16dc2b49129b2
mdemianchuk <nickdemianchuk@gmail.com>
2020-05-12 08:36:24 -0500

    Handle empty or whitespace properties in the settings.json

--- bot.py
+++ bot.py
@@ -4,9 +4,8 @@ START_URL = "https://www.supremenewyork.com/mobile#categories/new"
 
 
 class Bot:
-    def __init__(self, settings):
+    def __init__(self):
         self.webdriver = WebDriver()
-        self.settings = settings
 
     def start(self):
         self.webdriver.open_url(START_URL)
@@ -17,27 +16,21 @@ class Bot:
     def is_item_selected(self):
         return self.webdriver.get_current_url() != START_URL
 
-    def is_item_present(self):
-        return bool(self.webdriver.find_element_by_visible_text(self.settings["itemName"]))
-
-    def select_item(self):
-        item_name = self.settings["itemName"]
+    def select_item(self, item_name):
         while not self.is_item_selected():
             item = self.webdriver.find_element_by_visible_text(item_name)
             if not self.webdriver.click_on_element(item):
                 self.webdriver.refresh()
 
-    def select_colorway(self):
+    def select_colorway(self, item_colorway_position):
         colorway_box_x_path = '//*[@id="style-selector"]/li[{}]'.format(
-            self.settings["itemColorwayPosition"]
-        )
+            item_colorway_position)
         colorway_box = self.webdriver.find_element_by_x_path(
             colorway_box_x_path)
         return self.webdriver.click_on_element(colorway_box)
 
-    def select_size(self):
-        return self.webdriver.select_dropdown_option(
-            '//*[@id="size-options"]', self.settings["itemSize"])
+    def select_size(self, item_size):
+        return self.webdriver.select_dropdown_option('//*[@id="size-options"]', item_size)
 
     def add_to_cart(self):
         add_to_cart_button = self.webdriver.find_element_by_visible_text(
@@ -49,37 +42,37 @@ class Bot:
             "check out")
         return self.webdriver.click_on_element(checkout_button)
 
-    def fill_in_checkout_form(self):
+    def fill_in_checkout_form(self, billing_info):
         self.webdriver.fill_in_input_field(
-            '//*[@id="billing-info"]/tbody/tr[1]/td/input', self.settings["fullName"]
+            '//*[@id="billing-info"]/tbody/tr[1]/td/input', billing_info["fullName"]
         )
         self.webdriver.fill_in_input_field(
-            '//*[@id="billing-info"]/tbody/tr[2]/td/input', self.settings["email"]
+            '//*[@id="billing-info"]/tbody/tr[2]/td/input', billing_info["email"]
         )
         self.webdriver.fill_in_input_field(
-            '//*[@id="billing-info"]/tbody/tr[3]/td/input', self.settings["phone"]
+            '//*[@id="billing-info"]/tbody/tr[3]/td/input', billing_info["phone"]
         )
         self.webdriver.fill_in_input_field(
-            '//*[@id="billing-info"]/tbody/tr[4]/td/input', self.settings["address"]
+            '//*[@id="billing-info"]/tbody/tr[4]/td/input', billing_info["address"]
         )
-        if bool(self.settings["unit"]):
+        if bool(billing_info["unit"]):
             self.webdriver.fill_in_input_field(
-                '//*[@id="billing-info"]/tbody/tr[5]/td/input', self.settings["unit"]
+                '//*[@id="billing-info"]/tbody/tr[5]/td/input', billing_info["unit"]
             )
         self.webdriver.fill_in_input_field(
-            '//*[@id="address_inputs_table"]/tbody/tr/td[1]/input', self.settings["zip"]
+            '//*[@id="address_inputs_table"]/tbody/tr/td[1]/input', billing_info["zip"]
         )
         self.webdriver.select_dropdown_option(
-            '//*[@id="order_billing_state"]', self.settings["state"])
+            '//*[@id="order_billing_state"]', billing_info["state"])
         self.webdriver.fill_in_input_field(
-            '//input[@placeholder="credit card number"]', self.settings["ccNumber"]
+            '//input[@placeholder="credit card number"]', billing_info["ccNumber"]
         )
         self.webdriver.select_dropdown_option(
-            '//*[@id="credit_card_month"]', self.settings["expM"])
+            '//*[@id="credit_card_month"]', billing_info["expM"])
         self.webdriver.select_dropdown_option(
-            '//*[@id="credit_card_year"]', self.settings["expY"])
+            '//*[@id="credit_card_year"]', billing_info["expY"])
         self.webdriver.fill_in_input_field(
-            '//input[@placeholder="cvv"]', self.settings["cvv"])
+            '//input[@placeholder="cvv"]', billing_info["cvv"])
 
     def agree_with_terms(self):
         terms_checkbox = self.webdriver.find_element_by_x_path(
--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from bot import Bot
 
 
 if __name__ == "__main__":
-    bot = Bot(util.get_settings())
+    settings = util.get_settings()
+    item_name = settings["itemInfo"]["name"].strip()
+    item_colorway_position = settings["itemInfo"]["colorwayPosition"].strip()
+    item_size = settings["itemInfo"]["size"].strip()
+    billing_info = settings["billingInfo"]
+
+    bot = Bot()
     bot.start()
     start_time = time.time()
-    bot.select_item()
-    bot.select_colorway()
-    bot.select_size()
+    bot.select_item(item_name)
+    if item_colorway_position:
+        bot.select_colorway(item_colorway_position)
+    if item_size:
+        bot.select_size(item_size)
     bot.add_to_cart()
     bot.go_to_checkout()
-    bot.fill_in_checkout_form()
+    bot.fill_in_checkout_form(billing_info)
     bot.agree_with_terms()
     print(time.time() - start_time)
