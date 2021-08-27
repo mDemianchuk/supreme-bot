8c7fb69589a818df56854e44f761f18a3afd1232
mdemianchuk <nickdemianchuk@gmail.com>
2021-08-26 23:54:15 -0500

    Add error handling for file reading

--- utils/json_util.py
+++ utils/json_util.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 from utils.logging_util import log_error
 
 
@@ -6,5 +7,9 @@ def read_json_file(filename: str):
     try:
         with open(filename) as file:
             return json.load(file)
-    except:
-        log_error("Error while loading the settings file.")
+    except OSError:
+        log_error(f"Unable to read file {filename}.")
+    except JSONDecodeError:
+        log_error(f"Unable to decode JSON from {filename}.")
+    finally:
+        exit()
